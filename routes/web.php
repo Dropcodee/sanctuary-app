f91d6b9ccd7bf87e1817dc55414364fb65b100fe
Code Magic <classic@Dropcodes-Mac.local>
2021-02-28 20:31:10 +0100

    route changed

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/dashboard', function () {
     return view('dash.dashboard');
 })->name('dashboard');
 # ROUTES FOR DEPARTMENTS
-Route::group(function () {
+Route::middleware(['guest'])->group(function () {
     Route::get('/departments', [DepartmentController::class, 'index'])->name('departments');
     Route::get('/departments/create', [DepartmentController::class, 'create'])->name('create-department');
     Route::post('/departments/store', [DepartmentController::class, 'store'])->name('store-department');
@@ -32,7 +32,7 @@ Route::group(function () {
     Route::delete('/departments/{departmentId}/delete', [DepartmentController::class, 'destroy'])->name('delete-department');
 });
 
-Route::group(function () {
+Route::middleware(['guest'])->group(function () {
     Route::get('/excos', [ExcoController::class, 'index'])->name('excos');
     Route::post('/excos/store', [ExcoController::class, 'store'])->name('store-exco');
     Route::patch('/excos/{excoId}/update', [ExcoController::class, 'update'])->name('update-excos');
